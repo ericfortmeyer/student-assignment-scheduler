6d11e5b900f1115abfaede8395b7f044b41f696d
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2018-10-24 23:20:19 -0500

    Add function loading file

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+
+require "Functions/includeFilesInDirectory.php";
+
+/**
+ * Include files from the current directory
+ */
+TalkSlipSender\Functions\includeFilesInDirectory(__DIR__ . "/Functions");
