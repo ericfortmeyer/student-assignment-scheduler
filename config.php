c1ab51af48856dcbd9bb819a3ac82213dab28260
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2018-10-25 22:32:45 -0500

    Add schedule styling and recipients

--- config.php
+++ config.php
@@ -1,8 +1,19 @@
 <?php
 return [
     "from_email" => "lammo_huntsville@icloud.com",
-    "template" => "template/S-89-E.pdf",
+    "assignment_form_template" => "templates/S-89-E.pdf",
+    "schedule_template" => "templates/S-140-E.pdf",
     "assignment_forms_destination" => "data/forms",
+    "schedules_destination" => "data/schedules",
+    "schedule_recipients" => [
+        "Eric Fortmeyer",
+        "Bill Evis",
+        "Kevin Flint",
+        "Jonathan Johnson",
+        "Kyle Pockrus",
+        "Elliott Jolly"
+    ],
+    "schedule_font_size" => 10,
     "font" => "Helvetica",
     "font_color" => "blue",
     "assignment_mark" => "x",
@@ -26,6 +37,214 @@ return [
             255,
             0,
             0,
+        ]
+    ],
+    "schedule" => [
+        "week" => [
+            [
+                "date" => [
+                    20,
+                    38
+                ],
+                4 => [
+                    "position" => [
+                        20,
+                        67,
+                    ],
+                    "name" => [
+                        134,
+                        67
+                    ],
+                    "counsel_point" => [
+                        200,
+                        67
+                    ]
+                ],
+                5 => [
+                    "position" => [
+                        20,
+                        86,
+                    ],
+                    "name" => [
+                        134,
+                        86
+                    ],
+                    "counsel_point" => [
+                        200,
+                        86
+                    ]
+                ],
+                6 => [
+                    "position" => [
+                        20,
+                        93,
+                    ],
+                    "name" => [
+                        134,
+                        93
+                    ],
+                    "counsel_point" => [
+                        200,
+                        93
+                    ]
+                ],
+                7 => [
+                    "position" => [
+                        20,
+                        100,
+                    ],
+                    "name" => [
+                        134,
+                        100
+                    ],
+                    "counsel_point" => [
+                        200,
+                        100
+                    ]
+                ]
+            ],
+            [
+                "date" => [
+                    20,
+                    116
+                ],
+                4 => [
+                    "position" => [
+                        20,
+                        134,
+                    ],
+                    "name" => [
+                        134,
+                        134
+                    ],
+                ],
+                5 => [
+                    "position" => [
+                        20,
+                        153,
+                    ],
+                    "name" => [
+                        134,
+                        153
+                    ],
+                ],
+                6 => [
+                    "position" => [
+                        20,
+                        160,
+                    ],
+                    "name" => [
+                        134,
+                        160
+                    ],
+                ],
+                7 => [
+                    "position" => [
+                        20,
+                        167,
+                    ],
+                    "name" => [
+                        134,
+                        167
+                    ],
+                ]
+            ],    
+
+            [
+                "date" => [
+                    20,
+                    184
+                ],
+                4 => [
+                    "position" => [
+                        20,
+                        203,
+                    ],
+                    "name" => [
+                        134,
+                        203
+                    ],
+                ],
+                5 => [
+                    "position" => [
+                        20,
+                        222,
+                    ],
+                    "name" => [
+                        134,
+                        222
+                    ],
+                ],
+                6 => [
+                    "position" => [
+                        20,
+                        230,
+                    ],
+                    "name" => [
+                        134,
+                        230
+                    ],
+                ],
+                7 => [
+                    "position" => [
+                        20,
+                        237,
+                    ],
+                    "name" => [
+                        134,
+                        237
+                    ],
+                ]
+            ],
+            [
+                "date" => [
+                    20,
+                    32
+                ],
+                4 => [
+                    "position" => [
+                        20,
+                        51,
+                    ],
+                    "name" => [
+                        134,
+                        51
+                    ],
+                ],
+                5 => [
+                    "position" => [
+                        20,
+                        70,
+                    ],
+                    "name" => [
+                        134,
+                        70
+                    ],
+                ],
+                6 => [
+                    "position" => [
+                        20,
+                        77,
+                    ],
+                    "name" => [
+                        134,
+                        77
+                    ],
+                ],
+                7 => [
+                    "position" => [
+                        20,
+                        84,
+                    ],
+                    "name" => [
+                        134,
+                        84
+                    ],
+                ]
+            ],
+            // [
+
+            // ]
         ]
     ],
     "talk_slip" => [
