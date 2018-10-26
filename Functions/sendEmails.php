2ed81032e2868c8f9a23ffb16d16a5c2bef6850b
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2018-10-25 22:36:01 -0500

    Add schedule sending to sendEmails function

--- Functions/sendEmails.php
+++ Functions/sendEmails.php
@@ -9,43 +9,26 @@ use TalkSlipSender\Contact;
 function sendEmails(
     MailSender $MailSender,
     ListOfContacts $ListOfContacts,
-    array $config,
-    array $contacts
+    array $contacts,
+    string $assignment_forms_destination,
+    array $schedule_recipients,
+    string $schedule_filename
 ) {
-    /**
-     * Path to files that will be attached to the emails
-     */
-    $path_to_forms = __DIR__ . "/../{$config["assignment_forms_destination"]}";
-            
-    array_map(
-        function (string $file) use ($MailSender, $ListOfContacts, $contacts, $path_to_forms) {
-            try {
-                /**
-                 * A contact object
-                 * @var Contact $contact
-                 */
-                $contact = loadContacts($contacts, $ListOfContacts)
-                    ->getContactByFirstName(
-                        firstNameFromFilename($file)
-                    );
-    
-                $attachment = "$path_to_forms/$file";
-    
-                $MailSender
-                    ->addBody("Dear {$contact->firstName()},\r\n\r\nHere's your next assignment.\r\n\r\nThanks!")
-                    ->addAddress($contact->emailAddress(), $contact->fullname())
-                    ->addAttachment($attachment)
-                    ->send();
-    
-                echo "Email sent\r\n";
-            } catch (\Exception $e) {
-                echo "{$e->getMessage()}";
-                echo "failed\r\n";
-            }
-        },
-        array_diff(
-            scandir($path_to_forms),
-            [".", "..", ".DS_Store"]
-        )
+
+
+    sendAssignmentForms(
+        $MailSender,
+        $ListOfContacts,
+        $contacts,
+        __DIR__ . "/../{$assignment_forms_destination}"
+    );
+
+
+    sendSchedule(
+        $MailSender,
+        $ListOfContacts,
+        $schedule_recipients,
+        $contacts,
+        $schedule_filename
     );
 }
