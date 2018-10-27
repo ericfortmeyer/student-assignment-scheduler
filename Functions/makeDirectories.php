5b90e09bc62ec3d169fbaef4db176e5771ec622d
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2018-10-27 00:19:17 -0500

    Add makeDirectories function for bootstrapping

---
+++ Functions/makeDirectories.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace TalkSlipSender\Functions;
+
+function makeDirectories(array $directories): void
+{
+    array_map(
+        function (string $directory) {
+            !file_exists($directory)
+                && mkdir($directory, 0777, true);
+        },
+        $directories
+    );
+}
