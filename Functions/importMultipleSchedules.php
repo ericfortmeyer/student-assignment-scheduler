2ff09a886907b3deaafff9aeaa587cd5dbb5f742
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2018-10-27 22:18:14 -0500

    Remove ERROR_MSG const. Use variable instead.

--- Functions/importMultipleSchedules.php
+++ Functions/importMultipleSchedules.php
@@ -2,22 +2,18 @@
 
 namespace TalkSlipSender\Functions;
 
+
 use function TalkSlipSender\Functions\CLI\white;
 
 function importMultipleSchedules(string $path_to_json_schedules): array
 {
-    define(
-        "NO_SCHEDULES_ERROR_MSG",
-        white("Nothing was schedule. Bye\r\n")
-    );
-
     return array_map(
         function (string $json_file) use ($path_to_json_schedules) {
             return importJson("$path_to_json_schedules/$json_file");
         },
         filenamesInDirectory(
             $path_to_json_schedules,
-            NO_SCHEDULES_ERROR_MSG,
+            white("Nothing was scheduled.  Bye\r\n"),
             true
         )
     );
