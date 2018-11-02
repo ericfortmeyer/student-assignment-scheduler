8794cf8da973d6b23e99c55fba22399719fa7ac0
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2018-11-02 12:25:56 -0500

    Add import command

--- path_config.php
+++ path_config.php
@@ -4,5 +4,8 @@ return [
     "path_to_data" => "data",
     "path_to_workbooks" => "workbooks",
     "path_to_schedules" => "data/schedules",
-    "path_to_talkslip_template" => "template"
+    "path_to_assignments" => "data/assignments",
+    "path_to_forms" => "data/forms",
+    "path_to_talkslip_template" => "templates",
+    "path_to_templates" => "templates"
 ];
