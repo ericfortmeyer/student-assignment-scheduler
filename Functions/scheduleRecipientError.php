8a04d4e260652ba1ad720dacbfb58b393d0e1b46
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2018-11-24 22:45:36 -0600

    Minor refactoring

--- Functions/scheduleRecipientError.php
+++ Functions/scheduleRecipientError.php
@@ -8,5 +8,5 @@ function scheduleRecipientError(string $schedule_recipients_config_file)
         . PHP_EOL
         . "Who would you like to receive the full schedule each month?"
         . PHP_EOL
-        . "You can set it up in $schedule_recipients_config_file" . PHP_EOL;
+        . "You can add their name and email in $schedule_recipients_config_file" . PHP_EOL;
 }
