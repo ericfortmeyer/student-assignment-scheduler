a54df202047fe0c1f932763a1a8d87575b42b1ce
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2018-12-01 22:10:39 -0600

    Add absolute paths to registry.

--- FileRegistry/registry.php
+++ FileRegistry/registry.php
@@ -1,15 +1,15 @@
 <?php
 return [
-    "c61af8bc47f5dfad57cf7fc0965c356a0b8a9cf0" => "data/2018/December.json",
-    "ff295e06e34da21883a193537b22aaea0a7dd2f2" => "data/2018/November.json",
-    "0b55d2f3d733f7be198caeabfdce4457e671d362" => "data/2018/October.json",
-    "d436e1c1afa3b5ce663f1fa7ac49c4c604a6deca" => "data/assignments/2018/1108.json",
-    "8def709045cd38a24480090ed02d5e6b60aaf2b8" => "data/assignments/2018/1115.json",
-    "aba64f32999345ba26b0d9fb5f162fbcccde3fc6" => "data/assignments/2018/1122.json",
-    "46ae0c3051a313247afd59dd9bfb5c659d171f82" => "data/assignments/2018/1129.json",
-    "4a8861280dc5e96547d3e82a49ec7cbfef7e49ae" => "data/assignments/2018/1203.json",
-    "7db2a466c18a1d46de666f477e77054282434471" => "data/assignments/2018/1206.json",
-    "ad7d960343803e89e4ec1f7f6de355cd09fb8672" => "data/assignments/2018/1213.json",
-    "37c7259cd47ac6231fa08bf3c31e4a0f4cc03178" => "data/assignments/2018/1220.json",
-    "bead6b767af021770b2f56ee184b0ace93819fc0" => "data/assignments/2018/1227.json"
+    "c61af8bc47f5dfad57cf7fc0965c356a0b8a9cf0" => "/Users/Eric/Documents/Theocratic/LAMM/TalkSlipSender/data/2018/December.json",
+    "ff295e06e34da21883a193537b22aaea0a7dd2f2" => "/Users/Eric/Documents/Theocratic/LAMM/TalkSlipSender/data/2018/November.json",
+    "0b55d2f3d733f7be198caeabfdce4457e671d362" => "/Users/Eric/Documents/Theocratic/LAMM/TalkSlipSender/data/2018/October.json",
+    "d436e1c1afa3b5ce663f1fa7ac49c4c604a6deca" => "/Users/Eric/Documents/Theocratic/LAMM/TalkSlipSender/data/assignments/2018/1108.json",
+    "8def709045cd38a24480090ed02d5e6b60aaf2b8" => "/Users/Eric/Documents/Theocratic/LAMM/TalkSlipSender/data/assignments/2018/1115.json",
+    "aba64f32999345ba26b0d9fb5f162fbcccde3fc6" => "/Users/Eric/Documents/Theocratic/LAMM/TalkSlipSender/data/assignments/2018/1122.json",
+    "46ae0c3051a313247afd59dd9bfb5c659d171f82" => "/Users/Eric/Documents/Theocratic/LAMM/TalkSlipSender/data/assignments/2018/1129.json",
+    "4a8861280dc5e96547d3e82a49ec7cbfef7e49ae" => "/Users/Eric/Documents/Theocratic/LAMM/TalkSlipSender/data/assignments/2018/1203.json",
+    "7db2a466c18a1d46de666f477e77054282434471" => "/Users/Eric/Documents/Theocratic/LAMM/TalkSlipSender/data/assignments/2018/1206.json",
+    "ad7d960343803e89e4ec1f7f6de355cd09fb8672" => "/Users/Eric/Documents/Theocratic/LAMM/TalkSlipSender/data/assignments/2018/1213.json",
+    "37c7259cd47ac6231fa08bf3c31e4a0f4cc03178" => "/Users/Eric/Documents/Theocratic/LAMM/TalkSlipSender/data/assignments/2018/1220.json",
+    "bead6b767af021770b2f56ee184b0ace93819fc0" => "/Users/Eric/Documents/Theocratic/LAMM/TalkSlipSender/data/assignments/2018/1227.json"
 ];
