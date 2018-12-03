576d90b9e75885e7b38842829d54bbd94adc52c7
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2018-12-02 21:25:20 -0600

    Use absolute paths in path configuration file

--- config/path_config.php
+++ config/path_config.php
@@ -1,10 +1,13 @@
 <?php
 
+/**
+ * Using realpath means the directories must already be created
+ */
 return [
-    "path_to_data" => "data",
-    "path_to_workbooks" => "workbooks",
-    "path_to_schedules" => "data/schedules",
-    "path_to_assignments" => "data/assignments",
-    "path_to_forms" => "data/forms",
-    "path_to_templates" => "templates"
+    "path_to_data" => realpath(__DIR__ . "/../data"),
+    "path_to_workbooks" => realpath(__DIR__ . "/../workbooks"),
+    "path_to_schedules" => realpath(__DIR__ . "/../data/schedules"),
+    "path_to_assignments" => realpath(__DIR__ . "/../data/assignments"),
+    "path_to_forms" => realpath(__DIR__ . "/../data/forms"),
+    "path_to_templates" => realpath(__DIR__ . "/../Utils/templates")
 ];
