efbefa07f38a1c20d62b3dbe04d94a019ac315b0
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2018-12-10 15:48:04 -0600

    Add comments to regex

--- Functions/getAssignmentDate.php
+++ Functions/getAssignmentDate.php
@@ -8,18 +8,10 @@ function getAssignmentDate(string $text, string $month, string $interval_spec): 
 {
     $monthAllCaps = strtoupper($month);
 
-    list(
-        $beginning_of_line,
-        $one_or_more_horizontal_whitespaces,
-        $one_or_two_digits_captured,
-        $multiline_mode
-    ) = ["/^", "\h+", "(\d{1,2})", "/m"];
-
-    $pattern = $beginning_of_line
-        . $monthAllCaps
-        . $one_or_more_horizontal_whitespaces
-        . $one_or_two_digits_captured
-        . $multiline_mode;
+    $pattern = "(?# beginning of line)/^"
+        . "(?# one or more horizontal whitespaces)\h+"
+        . "(?# capture one or two digits)(\d{1,2})"
+        . "(?# multiline mode)/m";
 
     return date_create_immutable_from_format(
         "F d",
