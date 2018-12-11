2870a800bafc0aad946657926d519cf584f45055
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2018-12-10 19:28:12 -0600

    Add comments to regex

--- Functions/getAssignment.php
+++ Functions/getAssignment.php
@@ -2,21 +2,20 @@
 
 namespace TalkSlipSender\Functions;
 
-function getAssignment(int $number, string $text): string
+function getAssignment(int $assignment_number, string $text): string
 {
-    $does_not_have_schedule = strlen($text) < 400;
-    if ($does_not_have_schedule) {
-        return "";
-    }
     $string_representation_of_media_image_for_videos = "w";
-
+    
+    // example: 5 Initial Call (
     $pattern = "/(?# newline)[\n]"
-        . "(?# one or more tabs)[\h]"
+        // the numeric representation of the assignment
+        . "${assignment_number}"
+        . "(?# one or more horizontal whitespaces)[\h]"
         . "(?# capture if one or more non numerical characters)([^0-9]+)"
         . "(?# one or more horizontal whitespaces)[\h]"
-        . "(?# one open parentheses\(/";
+        . "(?# one open parentheses)\(/";
 
-        $result = ltrim(
+    $result = ltrim(
             parse(
                 $pattern,
                 $text
--- Functions/getAssignmentDate.php
+++ Functions/getAssignmentDate.php
@@ -6,13 +6,13 @@ use \DateInterval;
 
 function getAssignmentDate(string $text, string $month, string $interval_spec): string
 {
-    $monthAllCaps = strtoupper($month);
-
-    $pattern = "(?# beginning of line)/^"
+    // example: JANUARY 10
+    $pattern = "/^(?# beginning of line)"
+        . strtoupper($month)
         . "(?# one or more horizontal whitespaces)\h+"
         . "(?# capture one or two digits)(\d{1,2})"
         . "(?# multiline mode)/m";
-
+    
     return date_create_immutable_from_format(
         "F d",
         "$month " . parse(
