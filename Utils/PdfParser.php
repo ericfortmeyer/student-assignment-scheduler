c44c5437481f2f529e92b9409fa5559771bcf4e6
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2018-12-22 02:04:53 -0600

    Use parse configuration file so that parsers can be configured from the same file. Use \Ds\Map instead of array for cleaner coding.

--- Utils/PdfParser.php
+++ Utils/PdfParser.php
@@ -27,12 +27,30 @@ final class PdfParser implements ParserInterface
 
     public function getAssignments(string $textFromWorksheet, string $month, string $interval_spec): array
     {
+        
+        $parse_config = $this->getConfig();
+        $pattern_func = $parse_config["pdf_assignment_pattern_func"];
+        $assignment_date_pattern_func = $parse_config["assignment_date_pattern_func"];
+    
         $map = new Map();
-        $map->put("date", getAssignmentDate($textFromWorksheet, $month, $interval_spec));
-        $map->put(5, getAssignment(5, $textFromWorksheet));
-        $map->put(6, getAssignment(6, $textFromWorksheet));
-        $map->put(7, getAssignment(7, $textFromWorksheet));
+        $map->put(
+            "date",
+            getAssignmentDate(
+                $assignment_date_pattern_func($month),
+                $textFromWorksheet,
+                $month,
+                $interval_spec
+            )
+        );
+        $map->put(5, getAssignment($pattern_func(5), $textFromWorksheet));
+        $map->put(6, getAssignment($pattern_func(6), $textFromWorksheet));
+        $map->put(7, getAssignment($pattern_func(7), $textFromWorksheet));
 
         return $map->toArray();
+    }
+
+    protected function getConfig(): array
+    {
+        return include "parse_config.php";
     }
 }
