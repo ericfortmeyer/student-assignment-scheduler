c98b791eebfb17dc7f4a12a6f7a1b1da1bc1f344
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2018-12-22 13:58:03 -0600

    Add meeting night interval specification to configuration.

--- Utils/parse_config.php
+++ Utils/parse_config.php
@@ -9,17 +9,22 @@ $exclude_from_assignment_lookup = [
 $titles_to_exclude = implode("|", $exclude_from_assignment_lookup);
 
 return [
+    "interval_spec" => [
+        "Monday" => "P0D",
+        "Tuesday" => "P1D",
+        "Wednesday" => "P2D",
+        "Thursday" => "P3D",
+        "Friday" => "P4D"
+    ],
     "pdf_assignment_pattern_func" => function (int $assignment_number): string {
         return "/(?# newline)[\n]"
-        // the numeric representation of the assignment
-        . "${assignment_number}"
+        . "(?# the numeric representation of the assignment)${assignment_number}"
         . "(?# one or more horizontal whitespaces)[\h]"
         . "(?# capture if one or more non numerical characters)([^0-9]+)"
         . "(?# one or more horizontal whitespaces)[\h]"
         . "(?# one open parentheses)\(/";
     },
     "assignment_date_pattern_func" => function (string $month): string {
-        // example: JANUARY 10
         return "/^(?# beginning of line)"
         . strtoupper($month)
         . "(?# one or more horizontal whitespaces)\h+"
