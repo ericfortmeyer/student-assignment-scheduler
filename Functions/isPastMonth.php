ff577f280c4e2b75668ba0f67c4e4c7645587c2e
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2018-12-22 17:50:38 -0600

    Linting

--- Functions/isPastMonth.php
+++ Functions/isPastMonth.php
@@ -5,12 +5,12 @@ namespace TalkSlipSender\Functions;
 /**
  * The option to add the year is necessary since it may be late in the year
  * when comparing January or February for instance
- * 
+ *
  * @param string $month
  * @param string|null $year
  * @return bool
  */
 function isPastMonth(string $month, ?string $year = null): bool
-{    
+{
     return monthObj($month, $year) <= date_create("00:00");
 }
