30a5ad0af6f54def3ae5dd19c0e68525ff703a3d
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2018-12-23 11:22:52 -0600

    Create function to import schedules.  Add comments and minor refactoring to assign script.

---
+++ Functions/importSchedule.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace TalkSlipSender\Functions;
+
+/**
+ * Convert the data into a regular PHP array.
+ * 
+ * Remove any metadata in the Json resulting in
+ * an array containing only the information
+ * representing each week of the schedule.
+ */
+function importSchedule(string $path_to_schedules): array
+{
+    return weeksFrom(importJson($path_to_schedules));
+}
