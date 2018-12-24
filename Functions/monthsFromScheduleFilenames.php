cdb30f825f429954e63a0e78fa1feb35b7f4af11
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2018-12-24 01:24:03 -0600

    Move anonymous function definitions out of method calls to remove noise from the code.

--- Functions/monthsFromScheduleFilenames.php
+++ Functions/monthsFromScheduleFilenames.php
@@ -4,20 +4,33 @@ namespace TalkSlipSender\Functions;
 
 use \Ds\Vector;
 
-function monthsFromScheduleFilenames(string $path_to_json_schedules, bool $do_past_months = false): array
-{
-    $vector = new Vector(filenamesInDirectory($path_to_json_schedules));
+function monthsFromScheduleFilenames(
+    string $path_to_json_schedules,
+    ?int $year = null,
+    bool $do_past_months = false
+): array {
+
+    $removeExtension = function (string $filename): string {
+        return basename($filename, ".json");
+    };
+    $sortMonths = function (string $month_a, string $month_b) {
+        return monthObj($month_a) <=> monthObj($month_b);
+    };
+    $removePastMonthsIfRequested = function (string $month) use ($year, $do_past_months): bool {
+        return $do_past_months || !isPastMonth($month, $year);
+    };
+    $addKey = function (string $month): array {
+        return ["month" => $month];
+    };
+
     /**
      * Requires key "month" to simplify passing arguments into
      * sortMonths function for other clients
      */
-    return $vector->map(function (string $filename) {
-        return basename($filename, ".json");
-    })->sorted(function (string $month_a, string $month_b) {
-        return monthObj($month_a) <=> monthObj($month_b);
-    })->filter(function (string $month) use ($do_past_months) {
-        return !$do_past_months && !isPastMonth($month);
-    })->map(function (string $month) {
-        return ["month" => $month];
-    })->toArray();
+    return (new Vector(filenamesInDirectory($path_to_json_schedules)))
+        ->map($removeExtension)
+        ->sorted($sortMonths)
+        ->filter($removePastMonthsIfRequested)
+        ->map($addKey)
+        ->toArray();
 }
