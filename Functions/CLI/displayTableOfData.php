80bb9ee89a403e63d25110cd41bd2a0b23c0224c
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2018-12-24 01:25:58 -0600

    Use constants for clarity.

--- Functions/CLI/displayTableOfData.php
+++ Functions/CLI/displayTableOfData.php
@@ -2,8 +2,13 @@
 
 namespace TalkSlipSender\Functions\CLI;
 
+define("UNDERSCORE", "_");
+define("DASH", "-");
+define("BAR", "|");
+
 function displayTableOfData(array $data): bool
 {
+
 
     $values = $headers = "";
     $result = false;
@@ -14,31 +19,26 @@ function displayTableOfData(array $data): bool
             continue;
         }
 
-        if ($key !== "year") {
-            $padded = str_pad($key, strlen($value), " ", STR_PAD_BOTH);
-            $headers .= green("|") . " ${padded} ";
-        }
+        $padded = str_pad($key, strlen($value), " ", STR_PAD_BOTH);
+        $headers .= green(BAR) . " ${padded} ";
     }
-    $line = $headers ? str_repeat("-", (int) round(strlen($headers) * 0.64)) . "\r\n" : "";
+    $line = $headers ? str_repeat(DASH, (int) round(strlen($headers) * 0.645)) . PHP_EOL : "";
     print green($line);
 
     print $headers;
-    $headers ? print green("|\r\n") : null;
+    $headers ? print green(BAR . PHP_EOL) : null;
 
     foreach ($data as $key => $value) {
         if (is_array($value)) {
             $result = displayTableOfData($value);
             continue;
         }
-        if ($key == "year") {
-            continue;
-        }
         $val_padded = str_pad($value, strlen($key), " ", STR_PAD_BOTH);
-        $values .= green("|") . " ${val_padded} ";
+        $values .= green(BAR) . " ${val_padded} ";
     }
     print $values;
-    $values ? print green("|\r\n") : null;
-    print green(str_repeat("_", (int) round(strlen($headers) * 0.64))) . "\r\n";
+    $values ? print green(BAR . PHP_EOL) : null;
+    print green(str_repeat(UNDERSCORE, (int) round(strlen($headers) * 0.645))) . PHP_EOL;
 
     return !empty($data) || $result;
 }
