6be715f9b19cee34d9782678ce98f04850d7074b
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2019-01-03 22:32:19 -0600

    Minor refactor.

--- Functions/CLI/setupScheduleRecipients.php
+++ Functions/CLI/setupScheduleRecipients.php
@@ -6,7 +6,9 @@ use function StudentAssignmentScheduler\Functions\generateContactsFile;
 
 function setupScheduleRecipients(string $schedule_recipients_config_file)
 {
-    $message = "Who would you like to recieve the full schedule for each month?.  Are you ready to set that up";
+    $message = "Who would you like to recieve the full schedule for each month?." . PHP_EOL
+        . "Are you ready to set that up";
+
     $add_schedule_recipients_prompt = prompt($message);
 
     $should_add_contacts = readline($add_schedule_recipients_prompt);
