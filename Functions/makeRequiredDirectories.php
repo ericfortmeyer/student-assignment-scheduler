e51d87e3f64e5050bfca4ed80ac0d41172c680c2
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2019-01-03 22:35:48 -0600

    Do not prepend __DIR__ to the directory since only full paths will be provided.

--- Functions/makeRequiredDirectories.php
+++ Functions/makeRequiredDirectories.php
@@ -9,8 +9,8 @@ function makeRequiredDirectories(array $required_directories)
     $RequiredDirectories = new Vector($required_directories);
     
     $make_dir = function (string $dir) {
-        $path = __DIR__ . DIRECTORY_SEPARATOR . $dir;
-        file_exists($path) || mkdir($path, 0770, true);
+        file_exists($dir) || mkdir($dir, 0770, true);
     };
+
     $RequiredDirectories->map($make_dir);
 }
