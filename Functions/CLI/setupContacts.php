84e03eb73c9f1896032d1e460d62fc459cdfc6b8
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2019-01-04 21:06:45 -0600

    Add prompt flag to read command.

--- Functions/CLI/setupContacts.php
+++ Functions/CLI/setupContacts.php
@@ -23,7 +23,7 @@ function setupContacts(string $path_to_contacts_file, ?string $retry_message = n
     $contacts = new Set();
 
     $get_data_from_user = function (string $prompt, string $input_type): array {
-        $tuple = exec("read '$prompt' input; echo \$input");
+        $tuple = exec("read -p '$prompt' input; echo \$input");
         print PHP_EOL;
 
         return $input_type === "email_address"
