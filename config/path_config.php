674752b8d8a51e153333300b0a090196c92b5cd5
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2019-01-05 21:35:17 -0600

    Remove realpath function from path config since some directories will be created in the script.  Including the config file before the directories are created will cause errors.

--- config/path_config.php
+++ config/path_config.php
@@ -1,13 +1,13 @@
 <?php
 
 /**
- * Using realpath means the directories must already be created
+ * Using means the directories must already be created
  */
 return [
-    "path_to_data" => realpath(__DIR__ . "/../data"),
-    "path_to_workbooks" => realpath(__DIR__ . "/../workbooks"),
-    "path_to_schedules" => realpath(__DIR__ . "/../data/schedules"),
-    "path_to_assignments" => realpath(__DIR__ . "/../data/assignments"),
-    "path_to_forms" => realpath(__DIR__ . "/../data/forms"),
-    "path_to_templates" => realpath(__DIR__ . "/../Utils/templates")
+    "path_to_data" => __DIR__ . "/../data",
+    "path_to_workbooks" => __DIR__ . "/../workbooks",
+    "path_to_schedules" => __DIR__ . "/../data/schedules",
+    "path_to_assignments" => __DIR__ . "/../data/assignments",
+    "path_to_forms" => __DIR__ . "/../data/forms",
+    "path_to_templates" => __DIR__ . "/../Utils/templates"
 ];
