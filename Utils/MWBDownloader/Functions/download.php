d0c9e944f16f74d5e0c631770f0c5680ad53eca9
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2019-03-02 07:16:43 -0600

    Update download.php

--- Utils/MWBDownloader/Functions/download.php
+++ Utils/MWBDownloader/Functions/download.php
@@ -6,26 +6,26 @@ use StudentAssignmentScheduler\Utils\MWBDownloader\Config\DownloadConfig;
 use StudentAssignmentScheduler\Utils\MWBDownloader\Month;
 use StudentAssignmentScheduler\Utils\MWBDownloader\Utils\ApiService;
 
+/**
+ * @param Month $month
+ * @param DownloadConfig $config
+ * @throws \Exception
+ */
 function download(Month $month, DownloadConfig $config): void
 {
-    try {
-        $url = buildUrlUsingMonth(
-            $month,
-            $config->apiUrl,
-            $config->apiQueryParams
-        );
+    $url = buildUrlUsingMonth(
+        $month,
+        $config->apiUrl,
+        $config->apiQueryParams
+    );
 
-        $fileObj = createFileObject(
-            (new ApiService($config->apiOpts($url)))
-                ->getPayloadAsObject($url),
-            $config
-        );
+    $fileObj = createFileObject(
+        (new ApiService($config->apiOpts($url)))
+            ->getPayloadAsObject(),
+        $config
+    );
 
-
-        $fileObj
-            ->downloadToDirectory($config->workbook_download_destination)
-            ->handleFileValidation();
-    } catch (\Exception $e) {
-        echo $e->getMessage();
-    }
+    $fileObj
+        ->downloadToDirectory($config->workbook_download_destination)
+        ->handleFileValidation();
 }
