38173e69ae372ad20060b80014377eeb2b5f2011
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2019-04-07 21:30:06 -0500

    Add message notifying user if the contacts haven't been setup yet.

--- Functions/CLI/Commands/Contacts/main.php
+++ Functions/CLI/Commands/Contacts/main.php
@@ -6,6 +6,12 @@ use \Ds\Set;
 
 function main(string $path_to_contacts, string $command)
 {
+    if (!file_exists($path_to_contacts)) {
+        echo "It looks like the contacts haven't been setup." . PHP_EOL
+            . "Please set them up by running the assign command." . PHP_EOL;
+        return;
+    }
+
     $contacts = new Set(require $path_to_contacts);
 
     $orDefault = function (Set $contacts, string $path_to_contacts) {
