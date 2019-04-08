0092c466e8545df21b91d75f06b7c8fc7a075e06
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2019-04-07 23:57:36 -0500

    Fixed bug.  Wrong numeric key for assignments.

--- Functions/CLI/userCreatesWeekOfAssignments.php
+++ Functions/CLI/userCreatesWeekOfAssignments.php
@@ -12,6 +12,10 @@ use StudentAssignmentScheduler\Classes\{
 /**
  * Creates data representing a week of assignments
  */
+/**
+ * @suppress PhanTypeInvalidRightOperand
+ * @suppress PhanTypeInvalidUnaryOperandNumeric
+ */
 function userCreatesWeekOfAssignments(
     array $schedule_for_week,
     string $assignment_date,
@@ -20,33 +24,32 @@ function userCreatesWeekOfAssignments(
 ): array {
     echo blue("Schedule for ${assignment_date}\r\n");
     
-    return array_merge(
-        ["year" => $year],
-        // use the union operator (+) instead of array_merge in order to preserve numeric keys
-        [createBibleReading($assignment_date, $ListOfContacts)] +
-        array_map(
-            function (string $assignment) use ($assignment_date, $ListOfContacts) {
-                echo heading($assignment);
-                return createAssignment(
-                    $ListOfContacts,
-                    $assignment_date,
-                    $assignment,
-                    retryUntilFullnameIsValid(
-                        new Fullname(
-                            readline("Enter student's name: ")
+    // use the union operator (+) instead of array_merge in order to preserve numeric keys
+    return
+        ["year" => $year] +
+            + [createBibleReading($assignment_date, $ListOfContacts)]
+            + array_map(
+                function (string $assignment) use ($assignment_date, $ListOfContacts) {
+                    echo heading($assignment);
+                    return createAssignment(
+                        $ListOfContacts,
+                        $assignment_date,
+                        $assignment,
+                        retryUntilFullnameIsValid(
+                            new Fullname(
+                                readline("Enter student's name: ")
+                            ),
+                            $ListOfContacts
                         ),
-                        $ListOfContacts
-                    ),
-                    userAssignsAssistant($assignment, $ListOfContacts)
-                );
-            },
-            array_filter(
-                $schedule_for_week,
-                function (string $data, string $key) {
-                    return $key !== "date" && shouldMakeAssignment($data);
+                        userAssignsAssistant($assignment, $ListOfContacts)
+                    );
                 },
-                ARRAY_FILTER_USE_BOTH
-            )
-        )
-    );
+                array_filter(
+                    $schedule_for_week,
+                    function (string $data, string $key) {
+                        return $key !== "date" && shouldMakeAssignment($data);
+                    },
+                    ARRAY_FILTER_USE_BOTH
+                )
+            );
 }
