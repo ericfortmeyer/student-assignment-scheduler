f274534651deefaa421055f610e366a1771f7193
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2019-04-16 04:53:55 -0500

    Remove function runInstallationScriptIfNecessary.
    The function was used for bootstrapping in the assign command.
    It was created to install packages in the vendor folder thus creating
    the vendor folder.  A circular dependency was created, since the
    autoload script looks for the vendor folder and the function was
    requiring the autoload script.  Installation of packages should be
    done before including the autoload script.

--- Functions/Bootstrapping/runInstallationScriptIfNecessary.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace StudentAssignmentScheduler\Functions\Bootstrapping;
-
-function runInstallationScriptIfNecessary(string $path_to_vendor_folder): void
-{
-    file_exists($path_to_vendor_folder)
-        || system("sh bin/install.sh");
-}
