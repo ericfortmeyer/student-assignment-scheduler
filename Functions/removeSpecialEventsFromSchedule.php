ee8af9891f95dcd76b25ad8d281dc8c0eefcb608
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2019-04-20 22:02:56 -0500

    Add cloning of SpecialEventHistory to removeSpecialEvents... function.

--- Functions/removeSpecialEventsFromSchedule.php
+++ Functions/removeSpecialEventsFromSchedule.php
@@ -15,13 +15,19 @@ function removeSpecialEventsFromSchedule(
 ): MonthOfAssignments {
     return $schedule->filter(
         function (Date $dateOfWeek, WeekOfAssignments $week) use ($SpecialEvents): bool {
-            while ($SpecialEvents->hasFutureEvents()) {
-                $SpecialEvent = $SpecialEvents->history()->pop();
+
+            // we have to clone here since we will need to traverse
+            // the stack to check each date in the schedule
+            $CopyOfSpecialEvents = clone $SpecialEvents;
+
+            while ($CopyOfSpecialEvents->hasFutureEvents()) {
+                $SpecialEvent = $CopyOfSpecialEvents->history()->pop();
 
                 if ($dateOfWeek == $SpecialEvent->date()) {
                     // remove it
                     return false;
                 }
+
             }
             return true;
         }
