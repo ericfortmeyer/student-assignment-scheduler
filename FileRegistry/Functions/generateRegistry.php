b8e0e7fc63c3c48f8e0e7f8e56ceda7b345dfb9a
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2019-04-24 21:46:06 -0500

    Remove asJson option from function arguments.

--- FileRegistry/Functions/generateRegistry.php
+++ FileRegistry/Functions/generateRegistry.php
@@ -12,50 +12,45 @@ define("__ARROW__", "=>");
 define("__COMMA__", ",");
 define("__SEMICOLON__", ";");
 
-function generateRegistry(array $data, string $filename = "", $asJSON = false)
+function generateRegistry(array $data, string $filename = "")
 {
     $path = __DIR__ . "/../";
     $registry_filename = $filename ? $filename : "${path}/registry";
 
-    if ($asJSON === true) {
-        $file = "${registry_filename}.json";
-        file_put_contents($file, json_encode($data, JSON_PRETTY_PRINT));
-    } else {
-        $string = "";
-        $string .= __OPENING_TAG__;
+    $string = "";
+    $string .= __OPENING_TAG__;
+    $string .= PHP_EOL;
+    $string .= __RETURN__;
+    $string .= __SPACE__;
+    $string .= __OPENING_BRACKET__;
+
+    foreach ($data as $key => $val) {
         $string .= PHP_EOL;
-        $string .= __RETURN__;
+        $string .= __INDENT__;
+        $string .= withQuotes($key);
         $string .= __SPACE__;
-        $string .= __OPENING_BRACKET__;
-    
-        foreach ($data as $key => $val) {
-            $string .= PHP_EOL;
-            $string .= __INDENT__;
-            $string .= withQuotes($key);
-            $string .= __SPACE__;
-            $string .= __ARROW__;
-            $string .= __SPACE__;
-            $string .= is_array($val) ? valAsArray($val) : withQuotes($val);
-            $string .= __COMMA__;
-        }
-        
-        $string = rtrim($string, __COMMA__);
-    
-        $string .= PHP_EOL;
-        $string .= __CLOSING_BRACKET__;
-        $string .= __SEMICOLON__;
-        $file = $filename ? $filename : $registry_filename . ".php";
-
-        /**
-         * Necessary to run tests on builds without versioning the test registry.
-         *
-         * The registry is not committed since it exposes absolute paths
-         * of files in the registry.
-         */
-        createDirectoryOfRegistryIfNotExists($file);
-
-        file_put_contents($file, $string);
+        $string .= __ARROW__;
+        $string .= __SPACE__;
+        $string .= is_array($val) ? valAsArray($val) : withQuotes($val);
+        $string .= __COMMA__;
     }
+    
+    $string = rtrim($string, __COMMA__);
+
+    $string .= PHP_EOL;
+    $string .= __CLOSING_BRACKET__;
+    $string .= __SEMICOLON__;
+    $file = $filename ? $filename : $registry_filename . ".php";
+
+    /**
+     * Necessary to run tests on builds without versioning the test registry.
+     *
+     * The registry is not committed since it exposes absolute paths
+     * of files in the registry.
+     */
+    createDirectoryOfRegistryIfNotExists($file);
+
+    file_put_contents($file, $string);
 }
 
 function createDirectoryOfRegistryIfNotExists(string $file): void
