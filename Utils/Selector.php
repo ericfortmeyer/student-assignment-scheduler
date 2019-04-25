f2344b875cf89d38ba420388bebd5b345421d401
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2019-04-24 21:52:21 -0500

    Add codeCoverageIgnore annotations.

--- Utils/Selector.php
+++ Utils/Selector.php
@@ -9,14 +9,24 @@ use StudentAssignmentScheduler\Classes\Actions\{
 
 final class Selector
 {
+    /**
+     * @codeCoverageIgnore
+     */
     private function __construct()
     {
     }
 
+    /**
+     * @codeCoverageIgnore
+     */
     private function __clone()
     {
     }
-
+    
+    /**
+     * @covers Failure::__invoke
+     * @covers Success::__invoke
+     */
     public static function do(bool $test_result, Success $successAction, Failure $failureAction)
     {
         $selectedAction = $test_result ? $successAction : $failureAction;
