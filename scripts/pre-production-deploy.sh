f7928e09ea19e0055fdbc60275f704d8c8c6901b
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2019-05-03 20:27:14 -0500

    Test CI config.

--- scripts/pre-production-deploy.sh
+++ scripts/pre-production-deploy.sh
@@ -2,7 +2,7 @@
 
 git config --global push.default simple
 
-git config receive.denyCurrentBranch updateInstead
+git config receive.denyCurrentBranch ignore
 
 git remote add staging ssh://$2@$1:/home/$2/staging
 
