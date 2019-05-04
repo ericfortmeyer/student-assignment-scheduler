11615eff9f3776acc843003a65af7dbb3e600678
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2019-05-04 01:17:26 -0500

    Test CI config.

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env sh
 BRANCH=production
-TARGET_DIR=/home/$2/$branch
-BARE_REPO=/home/$2/$branch.git
+TARGET_DIR=/home/$2/$BRANCH
+BARE_REPO=/home/$2/$BRANCH.git
 
 git config --global push.default simple
-git remote add $branch ssh://$2@$1:$BARE_REPO
+git remote add $BRANCH ssh://$2@$1:$BARE_REPO
 
 if [ ! -d "$BARE_REPO" ]; then
     ssh $2@$1 "mkdir $BARE_REPO && git init --bare $BARE_REPO"
-    git push -f $branch HEAD:refs/heads/master
-    git push -f $branch HEAD:refs/heads/$branch
+    git push -f $BRANCH HEAD:refs/heads/master
+    git push -f $BRANCH HEAD:refs/heads/$BRANCH
 
-    ssh $2@$1 "git clone $BARE_REPO && cd $TARGET_DIR && git checkout $branch"
+    ssh $2@$1 "git clone $BARE_REPO && cd $TARGET_DIR && git checkout $BRANCH"
 else
-    git push -f $branch HEAD:refs/heads/$branch
-    ssh $2@$1 "cd $TARGET_DIR && git pull ../$branch.git"
+    git push -f $BRANCH HEAD:refs/heads/$BRANCH
+    ssh $2@$1 "cd $TARGET_DIR && git pull ../$BRANCH.git"
 fi
