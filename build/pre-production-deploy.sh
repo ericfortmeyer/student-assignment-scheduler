67d7a70489897ca64799c32214f6ef33f76499a0
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2019-05-04 00:32:59 -0500

    Test CI config.

--- build/pre-production-deploy.sh
+++ build/pre-production-deploy.sh
@@ -13,11 +13,11 @@ git remote add staging ssh://$2@$1:$BARE_REPO
 
 if [ ! -d "$BARE_REPO" ]; then
     ssh $2@$1 "mkdir $BARE_REPO && git init --bare $BARE_REPO"
-    git push -f staging master
-    git push -f staging staging
+    git push -f staging HEAD:refs/heads/master
+    git push -f staging HEAD:refs/heads/staging
 
     ssh $2@$1 "git clone $BARE_REPO && cd $TARGET_DIR && git checkout staging"
 else
-    git push -f staging staging
+    git push -f staging HEAD:refs/heads/staging
     ssh $2@$1 "cd $TARGET_DIR && git pull ../staging.git"
 fi
