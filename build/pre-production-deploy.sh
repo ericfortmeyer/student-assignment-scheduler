9cbb58bec4b1c18e2358f7cd83cf6941740ed7e5
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2019-05-04 07:17:53 -0500

    Test CI config.

--- build/pre-production-deploy.sh
+++ build/pre-production-deploy.sh
@@ -6,7 +6,7 @@ BARE_REPO=/home/$2/$BRANCH.git
 git config --global push.default simple
 git remote add $BRANCH ssh://$2@$1:$BARE_REPO
 
-if [ ! -d "$BARE_REPO" ]; then
+if ssh $2@$1 "[ ! -d $BARE_REPO ]"; then
     ssh $2@$1 "mkdir $BARE_REPO && git init --bare $BARE_REPO"
     git push -f $BRANCH HEAD:refs/heads/master
     git push -f $BRANCH HEAD:refs/heads/$BRANCH
@@ -14,5 +14,5 @@ if [ ! -d "$BARE_REPO" ]; then
     ssh $2@$1 "git clone $BARE_REPO && cd $TARGET_DIR && git checkout $BRANCH"
 else
     git push -f $BRANCH HEAD:refs/heads/$BRANCH
-    ssh $2@$1 "cd $TARGET_DIR && git pull ../$BRANCH.git"
+    ssh $2@$1 "cd $TARGET_DIR && git pull ../$BRANCH.git --no-ff"
 fi
