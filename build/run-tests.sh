7d21da783fa718dd19f7cf0f7e3d36230afea9b8
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2019-05-10 23:36:57 -0500

    Use custom mail server in tests.

--- build/run-tests.sh
+++ build/run-tests.sh
@@ -3,10 +3,19 @@
 VENDOR_DIR=$PWD/vendor
 PATH=$PATH:$VENDOR_DIR/bin
 
+MOCK_EMAIL_SERVER=$PWD/tests/mock-extern-service/mock-service
+MOCK_EMAIL_SERVER_VENDOR_DIR=$MOCK_EMAIL_SERVER/vendor
+
 if [ ! -d $VENDOR_DIR ]; then
     composer -q install
 fi
 
+if [ ! -d $MOCK_EMAIL_SERVER_VENDOR_DIR ]; then
+    cd ../tests/mock-extern-service
+    composer -q upgrade
+    cd ../../
+fi
+
 phpunit --testsuite default
 php -d sendmail_path=$VENDOR_DIR/bin/smtp-mock-server.php $VENDOR_DIR/bin/phpunit --testsuite integration
-phpunit --testsuite end-to-end
+php -d sendmail_path=$MOCK_EMAIL_SERVER/smtp-mock-service.php phpunit --testsuite end-to-end
