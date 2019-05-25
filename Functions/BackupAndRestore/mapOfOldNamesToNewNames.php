49912207942c83a4b825030c3e17f9c909b2b0ee
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2019-05-25 07:06:14 -0500

    Move restore functions to separate files.

---
+++ Functions/BackupAndRestore/mapOfOldNamesToNewNames.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace StudentAssignmentScheduler\Functions\BackupAndRestore;
+
+use \Ds\Map;
+use StudentAssignmentScheduler\Classes\File;
+
+function mapOfOldNamesToNewNames(RestoreConfig $config): Map
+{
+    $map = new Map();
+
+    $config->filesInTmpDir()->reduce(
+        function ($carry, string $oldname) use ($map, $config) {
+            $file = new File($oldname);
+            $newname = $config->newNameFromOldName($file);
+            recursivelyAddTargetFilenamesToMap(
+                $map,
+                $config,
+                (string) $file,
+                $newname
+            );
+        }
+    );
+
+    return $map;
+}
--- Functions/BackupAndRestore/restore.php
+++ Functions/BackupAndRestore/restore.php
@@ -2,26 +2,13 @@
 
 namespace StudentAssignmentScheduler\Functions\BackupAndRestore;
 
-use StudentAssignmentScheduler\Classes\{
-    RestoreConfig,
-    File,
-    Directory
-};
-
-use function StudentAssignmentScheduler\Functions\{
-    buildPath,
-    hashOfFile
-};
+use function StudentAssignmentScheduler\Functions\hashOfFile;
 
 use function StudentAssignmentScheduler\FileRegistry\Functions\registerFile;
 
 use \ZipArchive;
 
-use \Ds\{
-    Map,
-    Queue,
-    Stack
-};
+use \Ds\Queue;
 
 function restore(RestoreConfig $config): bool
 {
@@ -65,69 +52,5 @@ function restore(RestoreConfig $config): bool
     );
 
     return moveAllFilesOrRollback($file_moving_queue, $config);
-}
-
-function moveAllFilesOrRollback(Queue $file_moving_queue, RestoreConfig $config): bool
-{
-    $undo_actions = new Stack();
-
-    while (!$file_moving_queue->isEmpty()) {
-        $current_move = $file_moving_queue->pop();
-        $undoOrFalse = $current_move();
-        if ($undoOrFalse !== false) {
-            $undo = $undoOrFalse;
-            $undo_actions->push($undo);
-        } else {
-            rollback($undo_actions);
-            return false;
-        }
-    }
-
-    $config->removeTmpDir();
-    return true;
-}
-
-function rollback(Stack $undo_actions): void
-{
-    while (!$undo_actions->isEmpty()) {
-        $current_undo = $undo_actions->pop();
-        $current_undo();
-    }
-}
-
-function mapOfOldNamesToNewNames(RestoreConfig $config): Map
-{
-    $map = new Map();
-
-    $config->filesInTmpDir()->reduce(
-        function ($carry, string $oldname) use ($map, $config) {
-            $file = new File($oldname);
-            $newname = $config->newNameFromOldName($file);
-            recursivelyAddTargetFilenamesToMap(
-                $map,
-                $config,
-                (string) $file,
-                $newname
-            );
-        }
-    );
-
-    return $map;
-}
-function recursivelyAddTargetFilenamesToMap(Map $map, RestoreConfig $config, string $oldname, string $newname)
-{
-    is_dir($oldname)
-        ? (new Directory($oldname))->files()->reduce(
-            function ($carry, string $name_of_file_in_dir) use ($map, $config, $oldname, $newname) {
-                $current_directory = $oldname;
-                $target_directory = $newname;
-                $basename_of_file = basename($name_of_file_in_dir);
-                $original_filename = buildPath($current_directory, $basename_of_file);
-                $target_filename = buildPath($target_directory, $basename_of_file);
-                recursivelyAddTargetFilenamesToMap($map, $config, $original_filename, $target_filename);
-            }
-        )
-        : $map->put($oldname, $newname);
-
 }
 
