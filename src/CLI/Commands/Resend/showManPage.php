10863e1502a15e5ce150b420f7ef0b5ae7cb6e97
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2019-06-01 07:49:14 -0500

    Add resend command.
    Use resend command to resend assignment forms and schedules.

---
+++ src/CLI/Commands/Resend/showManPage.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace StudentAssignmentScheduler\CLI\Commands\Resend;
+
+function showManPage(): void
+{
+    print PHP_EOL;
+    print "NAME" . PHP_EOL;
+    print "\tresend" . PHP_EOL . PHP_EOL;
+    print "Usage:" . PHP_EOL;
+    print "\tresend assignments" . PHP_EOL;
+    print "\tresend schedule" . PHP_EOL;
+    print "\tresend help -- this help page" . PHP_EOL . PHP_EOL;
+    print "DESCRIPTION" . PHP_EOL;
+    print "\tUse to resend assignment forms and schedules." . PHP_EOL;
+    print "\tThe program will prompt you to:" . PHP_EOL;
+    print "\t\t(1) Choose what you would like to send [assignment forms or schedules]" . PHP_EOL;
+    print "\t\t(2) If you chose assignment forms... select the week that the assignment form falls in." . PHP_EOL;
+    print "\t\t(3) If you chose assignment forms... select the assignment in the week you selected." . PHP_EOL;
+    print PHP_EOL;
+}
---
+++ src/Querying/Functions/removeAssignmentCopies.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace StudentAssignmentScheduler\Querying\Functions;
+
+function removeAssignmentCopies(string $filename): bool {
+    // copies have ctime prepended to the filename
+    $pattern = "/^[[:digit:]]{2}_[[:digit:]]{4}.json$/";
+    $isNotACopy = preg_match($pattern, $filename) == 1;
+    return $isNotACopy;
+}
