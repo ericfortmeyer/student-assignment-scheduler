4d9a16e78021efd95b888d440effda47dfc32eb5
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2019-06-25 20:33:46 -0500

    Use ListOfScheduleRecipients when saving instead of an array.

--- src/CLI/addContacts.php
+++ src/CLI/addContacts.php
@@ -8,6 +8,7 @@ use \Ds\{
 };
 
 use StudentAssignmentScheduler\ListOfContacts;
+use StudentAssignmentScheduler\ListOfScheduleRecipients;
 
 use function StudentAssignmentScheduler\Encryption\Functions\{
     box,
@@ -103,13 +104,11 @@ function addContacts(string $path_to_contacts_file, string $key, array $prompts 
 function sensitiveData(Set $contacts, string $path_to_contacts_file, string $key)
 {
     return basename($path_to_contacts_file) === "schedule_recipients"
-        ? $contacts->merge(
-            new Set(
-                file_exists($path_to_contacts_file)
+        ? (new ListOfScheduleRecipients($contacts->toArray()))->union(
+            file_exists($path_to_contacts_file)
                     ? unbox($path_to_contacts_file, $key)
-                    : []
-            )
-        )->toArray()
+                    : new ListOfScheduleRecipients([])
+        )
         : (new ListOfContacts($contacts->toArray()))->union(
             file_exists($path_to_contacts_file)
                 ? new ListOfContacts(unbox($path_to_contacts_file, $key)->toArray())
