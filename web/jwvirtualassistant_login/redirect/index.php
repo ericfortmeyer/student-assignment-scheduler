b405615d20c13b8fbc9271d1ab57887179feb7d0
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2019-07-06 04:15:18 -0500

    Refactor redirect logic.

--- web/jwvirtualassistant_login/redirect/index.php
+++ web/jwvirtualassistant_login/redirect/index.php
@@ -15,12 +15,12 @@ define("ACCESS_TOKEN_KEY", getenv("ACCESS_TOKEN_KEY"));
 define("_400_ERROR_PAGE", getenv("_400_ERROR_PAGE"));
 define("RESOURCE_SERVER", getenv("RESOURCE_SERVER"));
 
-if (!requestIsValid($Request, AUTH_PARAM_KEY, ACCESS_TOKEN_KEY)) {
+if (requestIsValid($Request, AUTH_PARAM_KEY, ACCESS_TOKEN_KEY)) {
+    $access_token = json_decode($Request->getQueryParams()[AUTH_PARAM_KEY], true)[ACCESS_TOKEN_KEY];
+    setAuthorizationHeader($access_token);
+    redirect((new Uri(RESOURCE_SERVER)));
+} else {
     redirect(
         (new Uri())->withPath(_400_ERROR_PAGE)
     );
 }
-
-$access_token = json_decode($Request->getQueryParams()[AUTH_PARAM_KEY], true)[ACCESS_TOKEN_KEY];
-setAuthorizationHeader($access_token);
-redirect((new Uri(RESOURCE_SERVER)));
