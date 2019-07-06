35ca0f80687178007d05f6dc6f1eca257fb87da8
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2019-07-06 04:01:56 -0500

    Validate authorization response and redirect to resource server.

--- web/jwvirtualassistant_login/redirect/index.php
+++ web/jwvirtualassistant_login/redirect/index.php
@@ -3,25 +3,25 @@
 use GuzzleHttp\Psr7\ServerRequest;
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Uri;
+use Dotenv\Dotenv;
 
 require "vendor/autoload.php";
 
+$Dotenv = Dotenv::create(__DIR__);
+$Dotenv->load();
 $Request = ServerRequest::fromGlobals();
-$query_param_key = "authorizationResponse";
-$access_token_key = "access_token";
+define("AUTH_PARAM_KEY", getenv("AUTHORIZATION_RESPONSE_QUERY_PARAM_KEY"));
+define("ACCESS_TOKEN_KEY", getenv("ACCESS_TOKEN_KEY"));
+define("_400_ERROR_PAGE", getenv("_400_ERROR_PAGE"));
+define("RESOURCE_SERVER", getenv("RESOURCE_SERVER"));
+print_r($_ENV);
 
-if (!requestIsValid($Request, $query_param_key, $access_token_key)) {
+if (!requestIsValid($Request, AUTH_PARAM_KEY, ACCESS_TOKEN_KEY)) {
     redirect(
-        (new Uri(""))->withPath("/400ErrorPage.php")
+        (new Uri())->withPath(_400_ERROR_PAGE)
     );
 }
 
-$access_token = json_decode($Request->getQueryParams()[$query_param_key], true)[$access_token_key];
+$access_token = json_decode($Request->getQueryParams()[AUTH_PARAM_KEY], true)[ACCESS_TOKEN_KEY];
 setAuthorizationHeader($access_token);
-redirect(
-    (new Uri(""))
-        ->withScheme("http")
-        ->withHost("localhost")
-        ->withPort("8080")
-        ->withPath("templates-for-incomplete-months-of-assignments")
-);
+redirect((new Uri(RESOURCE_SERVER)));
