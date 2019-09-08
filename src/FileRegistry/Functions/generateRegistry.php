cf1deaa9e4ae030e53cf7a60779c9e8d7b2e44ed
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2019-09-08 06:06:58 -0500

    Remove valAsArray function.

--- src/FileRegistry/Functions/generateRegistry.php
+++ src/FileRegistry/Functions/generateRegistry.php
@@ -16,7 +16,6 @@ function generateRegistry(array $data, string $filename = "")
 {
     $path = __DIR__ . "/../";
     $registry_filename = $filename ? $filename : "${path}/registry";
-
     $string = "";
     $string .= __OPENING_TAG__;
     $string .= PHP_EOL;
@@ -25,7 +24,6 @@ function generateRegistry(array $data, string $filename = "")
     $string .= __RETURN__;
     $string .= __SPACE__;
     $string .= __OPENING_BRACKET__;
-
     foreach ($data as $key => $val) {
         $string .= PHP_EOL;
         $string .= __INDENT__;
@@ -33,12 +31,10 @@ function generateRegistry(array $data, string $filename = "")
         $string .= __SPACE__;
         $string .= __ARROW__;
         $string .= __SPACE__;
-        $string .= is_array($val) ? valAsArray($val) : withQuotes($val);
+        $string .= withQuotes($val);
         $string .= __COMMA__;
     }
-    
     $string = rtrim($string, __COMMA__);
-
     $string .= PHP_EOL;
     $string .= __CLOSING_BRACKET__;
     $string .= __SEMICOLON__;
@@ -51,43 +47,16 @@ function generateRegistry(array $data, string $filename = "")
      * of files in the registry.
      */
     createDirectoryOfRegistryIfNotExists($file);
-
     file_put_contents($file, $string);
 }
 
 function createDirectoryOfRegistryIfNotExists(string $file): void
 {
     $directory_of_registry = dirname($file);
-    !file_exists($directory_of_registry)
-        && mkdir($directory_of_registry, 0700);
+    @mkdir($directory_of_registry, 0700);
 }
 
 function withQuotes(string $string): string
 {
-    return "\"" . $string . "\"";
-}
-
-function valAsArray(array $data): string
-{
-    $string = "";
-    $string .= __OPENING_BRACKET__;
-    $string .= PHP_EOL;
-    $string .= __INDENT__;
-    $string .= __INDENT__;
-    foreach ($data as $key => $val) {
-        $string .= withQuotes($key);
-        $string .= __SPACE__;
-        $string .= __ARROW__;
-        $string .= __SPACE__;
-        $string .= withQuotes($val);
-        $string .= __COMMA__;
-        $string .= PHP_EOL;
-        $string .= __INDENT__;
-        $string .= __INDENT__;
-    }
-    $string = rtrim(trim($string), __COMMA__ . PHP_EOL);
-    $string .= PHP_EOL;
-    $string .= __INDENT__;
-    $string .= __CLOSING_BRACKET__;
-    return $string;
+    return "\"${string}\"";
 }
