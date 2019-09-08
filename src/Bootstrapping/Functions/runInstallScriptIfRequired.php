dcbf58a0b9a6cd3081535de36ecd75cc30f0ad50
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2019-09-08 06:13:16 -0500

    Minor refactoring.

--- src/Bootstrapping/Functions/runInstallScriptIfRequired.php
+++ src/Bootstrapping/Functions/runInstallScriptIfRequired.php
@@ -8,9 +8,7 @@ function runInstallScriptIfRequired(string $vendor_directory_name, string $insta
         realpath(dirname($vendor_directory_name)),
         basename($vendor_directory_name)
     );
-
-    $installation_script_with_arg = realpath($installation_script_path) . " $installation_script_arg";
-
+    $installation_script_with_arg = realpath($installation_script_path) . " ${installation_script_arg}";
     \file_exists($vendor_directory_name)
-        || \system("bash $installation_script_with_arg");
+        || \system("bash ${installation_script_with_arg}");
 }
