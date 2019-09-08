0b6fb28b4846b2ac7c4ce71e14ab5588add5d8a9
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2019-09-08 18:19:47 -0500

    Add documenting comments.

--- src/Parsing/DocumentWrapper.php
+++ src/Parsing/DocumentWrapper.php
@@ -2,6 +2,14 @@
 
 namespace StudentAssignmentScheduler\Parsing;
 
+/**
+ * Use this wrapper as an interface to document objects.
+ * 
+ * Since an instance of a document object could be a
+ * result of a 3rd party library or a module in this application
+ * parsing a document, this wrapper ensures that an API will be available
+ * to it's client.
+ */
 class DocumentWrapper
 {
     /**
@@ -9,21 +17,43 @@ class DocumentWrapper
      */
     protected $document;
 
+    /**
+     * Create the instance.
+     * 
+     * @param mixed $document
+     */
     public function __construct($document)
     {
         $this->document = $document;
     }
 
+    /**
+     * Return an array of page objects from
+     * the document.
+     * 
+     * @return object[]
+     */
     public function getPages(): array
     {
         return $this->document->getPages();
     }
 
+    /**
+     * Return the content of the document.
+     * @codeCoverageIgnore
+     * 
+     * @return string
+     */
     public function getText(): string
     {
         return $this->document->getText();
     }
 
+    /**
+     * Return an array of details
+     * about the document.
+     * @return array
+     */
     public function getDetails(): array
     {
         return $this->document->getDetails();
